7b8a7a0e0b7175ae4f715963827839dedc66b606
Benjamin Jones <bljones36@gmail.com>
2018-09-14 22:47:48 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,9 @@ attr_accessor :name
 
   def match(array)
     matches = []
-    anagram = @name.split(/\w/) 
+    anagram = @name.split
     array.collect do |word|
-    letters = word.split(/\w/)
+    letters = word.split
     if letters.sort == anagram.sort
       matches << word
     end
